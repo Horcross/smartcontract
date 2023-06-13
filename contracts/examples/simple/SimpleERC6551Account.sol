c80f27c810ea117b1f9f296e90ab77453276473f
johnny890715 <johnny890715@gmail.com>
2023-06-13 14:51:46 +0800

    add: all the required in EIP-6551

--- contracts/examples/simple/SimpleERC6551Account.sol
+++ contracts/examples/simple/SimpleERC6551Account.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 
-import "./interfaces/IERC6551Account.sol";
-import "./lib/ERC6551AccountLib.sol";
+import "../../interfaces/IERC6551Account.sol";
+import "../../lib/ERC6551AccountLib.sol";
 
 contract SimpleERC6551Account is IERC165, IERC1271, IERC6551Account {
     uint256 public nonce;
